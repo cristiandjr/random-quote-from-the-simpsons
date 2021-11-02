e3a1758b4957a61b250c880c821b002ef141ccbd
cshjdr <cristiandjr89@gmail.com>
2021-11-02 12:42:34 -0300

    se agrega axios, se agregan hook, se agrega la api, se consume la api, se cae la api, pero se deja la estructura para luego probar el funcionamiento y agregar los estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
 
 function App() {
+
+  const [frase, guardarFrase] = useState({});
+
+  const {character, image, quote} = frase;
+
+  const api = async () => {
+    const url = 'https://thesimpsonsquoteapi.glitch.me/quotes';
+    const respuesta = await axios.get(url);
+
+    //console.log(respuesta.data[0]);
+    guardarFrase(respuesta.data[0]);
+  }
+
+  useEffect(() => {
+    api();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{character}</h1>
+      <img src={image} alt={character} />
+      <p>{quote}</p>
+      <button onClick={() => api() }>Random</button>
     </div>
   );
 }
